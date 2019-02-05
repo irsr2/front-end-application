d5c4dd84c6cd70d9fac70068f74fdf5bacb9f3d7
stellarluminant <stellarluminant@gmail.com>
2019-02-05 15:19:59 -0800

    Reorganize file structure

--- app/src/App.js
+++ app/src/App.js
@@ -1,7 +1,7 @@
 import React from 'react';
-import { Route } from 'react-router-dom';
+import { Route, Redirect } from 'react-router-dom';
 import Navbar from './components/Navbar';
-import BoardView from './views/BoardView';
+import HomeView from './views/HomeView';
 import IssueView from './views/IssueView';
 
 class App extends React.Component {
@@ -10,7 +10,7 @@ class App extends React.Component {
       <div id="page-wrapper">
         <Navbar isBoard="true"/>
         <article id="main">
-          <Route exact path='/' component={() => <BoardView/>} />
+          <Route exact path='/' component={() => <HomeView/>} />
           <Route path='/issue/:id' render={(props) => <IssueView id={props.match.params.id} {...props} />} />
         </article>
       </div>
